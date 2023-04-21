069d41caffdf909d1600918cda432b3f32061bf3
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-04-21 23:50:39 +0600

    responsive

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,12 +4,12 @@ import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
 import "./Blog.css";
 import { toast } from "react-toastify";
 
-const Blog = ({ post,handleWatchTime,handleBookMark }) => {
+const Blog = ({ post, handleWatchTime, handleBookMark }) => {
   const { url, img, name, watchTime, date, title, blogTitle } = post;
   return (
     <div className="data-card card">
       <div className="data-photo">
-        <img className="w-50" src={url} alt="" />
+        <img className="w-75" src={url} alt="" />
       </div>
 
       <div className="time d-flex justify-content-around align-items-center mt-3">
@@ -18,18 +18,16 @@ const Blog = ({ post,handleWatchTime,handleBookMark }) => {
         <p>Watch Time : {watchTime} </p>
 
         <FontAwesomeIcon
-            onClick={() => handleBookMark(post)}
+          onClick={() => handleBookMark(post)}
           icon={faBookBookmark}
         />
       </div>
       <p className="ms-5">Data:{date}</p>
       <h6 className="ms-5">{title} </h6>
       <h4 className="ms-5">{blogTitle} </h4>
-      <button
-        onClick={() => handleWatchTime(post.watchTime)}
-        className="btn btn-info w-25 mb-3"
-      >
-        Mark as read
+      <button className="btn btn-info w-25 mt-3 mb-3"
+        onClick={() => handleWatchTime(post.watchTime)}>
+        <span className="text-rose-50 font-semibold">Mark as read</span>
       </button>
     </div>
   );
